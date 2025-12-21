ca69ec10860f630ef664d46e3389d3c118a175cd
Eduardo <duba.noda737@gmail.com>
2025-12-21 18:11:40 -0400

    initial commit

---
+++ src/test/java/br/com/paymentsystem/demo/PaymentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paymentsystem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
