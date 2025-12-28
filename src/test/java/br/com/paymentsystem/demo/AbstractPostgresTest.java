4f110dced6bc5f8efbbd801249decd781d6c6e3d
Eduardo <duba.noda737@gmail.com>
2025-12-27 23:08:44 -0400

    test(infra): adicionar base de testes com PostgreSQL via Testcontainers

---
+++ src/test/java/br/com/paymentsystem/demo/AbstractPostgresTest.java
@@ -0,0 +1,30 @@
+package br.com.paymentsystem.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("test")
+public class AbstractPostgresTest {
+
+    @Container
+    static PostgreSQLContainer postgres =
+            new PostgreSQLContainer("postgres:16")
+                    .withDatabaseName("payment_test")
+                    .withUsername("pgsql")
+                    .withPassword("pgsql");
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+}
