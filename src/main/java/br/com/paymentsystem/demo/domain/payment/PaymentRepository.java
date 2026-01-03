8a46f2e4844dfdd37549ae3dc20f62d9e4339910
Eduardo <duba.noda737@gmail.com>
2026-01-03 14:43:28 -0400

    chore(PaymentRepository): adicionado query para para processar pagamentos

--- src/main/java/br/com/paymentsystem/demo/domain/payment/PaymentRepository.java
+++ src/main/java/br/com/paymentsystem/demo/domain/payment/PaymentRepository.java
@@ -1,9 +1,28 @@
 package br.com.paymentsystem.demo.domain.payment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.OffsetDateTime;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByIdempotencyKey(String key);
+    @Modifying
+    @Query("""
+        update Payment p
+           set p.leaseExpiresAt = :leaseUntil
+         where p.idempotencyKey = :key
+           and (p.leaseExpiresAt is null or p.leaseExpiresAt < :now)
+    """)
+    int tryAcquireLease(@Param("key")String paymentId, @Param("now") OffsetDateTime now, @Param("leaseUntil") OffsetDateTime ttl);
+    @Modifying
+    @Query("""
+    update Payment p
+       set p.status = :status
+     where p.idempotencyKey = :key
+""")
+    int requestStatusTransaction(@Param("key") String paymentId, @Param("status") PaymentStatus nextStatus);
 }
