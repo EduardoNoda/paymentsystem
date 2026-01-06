d934b02baabe995ba0dd09a8e18e40eff231c5f5
Eduardo <duba.noda737@gmail.com>
2026-01-06 16:48:07 -0400

    fix(PaymentGateway): adaptado interface PaymentGateway para atender aos requisitos do DTO PaymentGatewayRequest

--- src/main/java/br/com/paymentsystem/demo/application/payment/port/PaymentGateway.java
+++ src/main/java/br/com/paymentsystem/demo/application/payment/port/PaymentGateway.java
@@ -1,9 +1,9 @@
 package br.com.paymentsystem.demo.application.payment.port;
 
-import br.com.paymentsystem.demo.domain.payment.Payment;
+import br.com.paymentsystem.demo.infrastructure.dto.PaymentGatewayRequest;
 
 public interface PaymentGateway {
 
-    GatewayResult process(Payment payment);
+    GatewayResult process(PaymentGatewayRequest payment);
 
 }
