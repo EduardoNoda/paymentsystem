0004a33f176c39bc2cc1532d7de0448c3ddf793d
Eduardo <duba.noda737@gmail.com>
2026-01-08 09:50:04 -0400

    feat(PaymentRepository): adicionado método findProcessingWithExpiredLease com a função de identificar solicitações com o lease expirado para o job do sistema

--- src/main/java/br/com/paymentsystem/demo/domain/payment/PaymentRepository.java
+++ src/main/java/br/com/paymentsystem/demo/domain/payment/PaymentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
@@ -25,4 +26,13 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
      where p.idempotencyKey = :key
 """)
     int requestStatusTransaction(@Param("key") String paymentId, @Param("status") PaymentStatus nextStatus);
+
+    @Query("""
+            SELECT p
+            FROM Payment p
+            WHERE p.status = 'PROCESSING'
+            AND p.leaseExpiredAt IS NOT NULL
+            AND p.leaseExpiredAt < :now
+            """)
+    List<Payment> findProcessingWithExpiredLease(@Param("now") OffsetDateTime now);
 }
