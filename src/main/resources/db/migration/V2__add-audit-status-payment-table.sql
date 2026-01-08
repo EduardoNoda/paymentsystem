eef7c61b77cdd76b503101ce470b366aefcad9b5
Eduardo <duba.noda737@gmail.com>
2026-01-08 10:28:12 -0400

    feat(V2__add-audit-status-payment-table.sql): criado indice composto para otimização de consultas no banco

--- src/main/resources/db/migration/V2__add-audit-status-payment-table.sql
+++ src/main/resources/db/migration/V2__add-audit-status-payment-table.sql
@@ -4,7 +4,7 @@ CREATE TABLE payment_audit (
     CONSTRAINT fk_payment_id FOREIGN KEY (payment_id) REFERENCES payment(id),
     status VARCHAR(32) NOT NULL,
     action_origin VARCHAR(32) NOT NULL,
-    description TEXT NOT NULL,
+    description TEXT NULL,
     occurred_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
 
     CONSTRAINT chk_payment_audit_status
@@ -24,4 +24,7 @@ CREATE TABLE payment_audit (
     CHECK (
         action_origin IN ('API', 'JOB', 'ADMIN')
     )
+
+    CREATE INDEX idx_payment_audit_payment_id
+    ON payment_audit(payment_id, occurred_at DESC);
 );
