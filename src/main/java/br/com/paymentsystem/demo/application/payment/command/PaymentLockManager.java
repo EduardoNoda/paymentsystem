b46576e5dc98b244682370f0fb071c2dd8635c9e
Eduardo <duba.noda737@gmail.com>
2026-01-21 12:42:47 -0400

    refactor(FakePaymentGateway): retirada função não usada.

--- src/main/java/br/com/paymentsystem/demo/application/payment/command/PaymentLockManager.java
+++
@@ -1,5 +0,0 @@
-package br.com.paymentsystem.demo.application.payment.command;
-
-public interface PaymentLockManager {
-    boolean tryAcquire(String idempotencyKey);
-}
--- src/main/java/br/com/paymentsystem/demo/infrastructure/gateway/FakePaymentGateway.java
+++ src/main/java/br/com/paymentsystem/demo/infrastructure/gateway/FakePaymentGateway.java
@@ -3,14 +3,12 @@ package br.com.paymentsystem.demo.infrastructure.gateway;
 import br.com.paymentsystem.demo.application.payment.port.GatewayResult;
 import br.com.paymentsystem.demo.application.payment.port.PaymentGateway;
 import br.com.paymentsystem.demo.infrastructure.dto.PaymentGatewayRequest;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FakePaymentGateway implements PaymentGateway {
 
-    private GatewayResult nextResult = GatewayResult.APPROVED;
-
-    public void simulateNextResult (GatewayResult result) {
-        this.nextResult = result;
-    }
+    private final GatewayResult nextResult = GatewayResult.APPROVED;
 
 
     public GatewayResult process(PaymentGatewayRequest request) {
