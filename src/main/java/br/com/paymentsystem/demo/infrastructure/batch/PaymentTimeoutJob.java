ef957fb50bd478400e5e46f367b8cbf753930fb2
Eduardo <duba.noda737@gmail.com>
2026-01-21 12:55:19 -0400

    refactor(PaymentTimeoutJob): por não ser mais necessário settar origem e status via domínio, foi necessária as devidas alterações para se adaptar com o banco

--- src/main/java/br/com/paymentsystem/demo/infrastructure/batch/PaymentTimeoutJob.java
+++ src/main/java/br/com/paymentsystem/demo/infrastructure/batch/PaymentTimeoutJob.java
@@ -1,51 +1,55 @@
 package br.com.paymentsystem.demo.infrastructure.batch;
 
 import br.com.paymentsystem.demo.application.payment.port.ActionOrigin;
+import br.com.paymentsystem.demo.application.payment.port.ActionOriginContext;
 import br.com.paymentsystem.demo.domain.payment.Payment;
 import br.com.paymentsystem.demo.domain.payment.PaymentLeasePolicy;
 import br.com.paymentsystem.demo.domain.payment.PaymentRepository;
-import br.com.paymentsystem.demo.domain.payment.PaymentStatus;
-import jakarta.transaction.Transactional;
+import br.com.paymentsystem.demo.infrastructure.transaction.ActionOriginTransaction;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.List;
 
 @Component
 public class PaymentTimeoutJob {
 
-    private PaymentRepository paymentRepository;
-    private PaymentLeasePolicy paymentLeasePolicy;
+    private final PaymentRepository paymentRepository;
+    private final PaymentLeasePolicy paymentLeasePolicy;
+    private final ActionOriginContext actionOriginContext;
+    private final ActionOriginTransaction actionOriginTransaction;
 
     public PaymentTimeoutJob(
             PaymentRepository paymentRepository,
-            PaymentLeasePolicy paymentLeasePolicy
+            PaymentLeasePolicy paymentLeasePolicy,
+            ActionOriginContext actionOriginContext,
+            ActionOriginTransaction actionOriginTransaction
     ) {
         this.paymentRepository = paymentRepository;
         this.paymentLeasePolicy = paymentLeasePolicy;
+        this.actionOriginContext = actionOriginContext;
+        this.actionOriginTransaction = actionOriginTransaction;
     }
 
-    @Transactional
     @Scheduled(fixedDelay = 60_000)
-    public void execute() {
-
-        OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
-
-        List<Payment> expiredProcessingPayment = paymentRepository.findProcessingWithExpiredLease(now);
-
-        for(Payment payment : expiredProcessingPayment) {
-
-            payment.setStatus(PaymentStatus.TO_ANALYZE);
-
-            OffsetDateTime leaseUntil = paymentLeasePolicy.calculateLeaseUntil(now, ActionOrigin.JOB);
-
-            payment.setLeaseExpiresAt(leaseUntil);
-
-            paymentRepository.save(payment);
-        }
-
+    public void execute () {
+        actionOriginContext.set(ActionOrigin.JOB);
+        runJob();
     }
+
+    void runJob() {
+
+
+        List<Payment> expiredProcessingPayment =
+                paymentRepository.findProcessingWithExpiredLease();
+
+        for (Payment payment : expiredProcessingPayment) {
+
+            int updateRows = paymentRepository.forceToAnalyze(
+                    payment.getIdempotencyKey()
+            );
+        }
+    }
+
 
 }
