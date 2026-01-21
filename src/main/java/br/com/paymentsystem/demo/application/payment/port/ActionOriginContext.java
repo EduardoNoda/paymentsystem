7d70c273cc10b72522798c34584fa83460805f05
Eduardo <duba.noda737@gmail.com>
2026-01-21 12:34:30 -0400

    feat(ActionOriginContext): adicionado função get para determinar controle total de banco

--- src/main/java/br/com/paymentsystem/demo/application/payment/port/ActionOriginContext.java
+++ src/main/java/br/com/paymentsystem/demo/application/payment/port/ActionOriginContext.java
@@ -1,6 +1,6 @@
 package br.com.paymentsystem.demo.application.payment.port;
 
-@FunctionalInterface
 public interface ActionOriginContext {
     void set(ActionOrigin origin);
+    ActionOrigin get();
 }
