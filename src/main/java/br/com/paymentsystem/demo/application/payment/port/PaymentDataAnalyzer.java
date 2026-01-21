aedb69be883fffae30112dd95f55f1536c5bf445
Eduardo <duba.noda737@gmail.com>
2026-01-21 12:48:47 -0400

    fix(PaymentDataAnalyzer): resolvido conflito de dado sendo passado pelo ProcessPaymentUseCase

--- src/main/java/br/com/paymentsystem/demo/application/payment/port/PaymentDataAnalyzer.java
+++ src/main/java/br/com/paymentsystem/demo/application/payment/port/PaymentDataAnalyzer.java
@@ -1,10 +1,10 @@
 package br.com.paymentsystem.demo.application.payment.port;
 
-import br.com.paymentsystem.demo.domain.payment.Payment;
 import br.com.paymentsystem.demo.domain.payment.PaymentStatus;
+import br.com.paymentsystem.demo.infrastructure.dto.GatewayData;
 
 public interface PaymentDataAnalyzer {
 
-    PaymentStatus dataAnalyzer(Payment payment);
+    PaymentStatus dataAnalyzer(GatewayData payment);
 
 }
